e65b1c483868e184fae4e08bf1263d73ac157d05
Lucia Llopis <luciallopis@gmail.com>
2025-01-14 15:13:06 +0100

    añado Hola mundo

--- miclase1.java
+++ miclase1.java
@@ -8,3 +8,13 @@ public class MiClase1
         }
     }
 }
+public class MiClase1
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("Hola mundo");
+        }
+    } .gitignore
+}
